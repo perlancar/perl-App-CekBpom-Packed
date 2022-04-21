231cb25d35a29fb3bd6aaacd187d2cb5e1580210
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-04-21 14:16:27 +0700

    Initial commit

---
+++ lib/App/CekBpom/Packed.pm
@@ -0,0 +1,23 @@
+package App::CekBpom::Packed;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+1;
+# ABSTRACT: Packed version of cek-bpom-* scripts
+
+=head1 DESCRIPTION
+
+This distribution provides the packed version of scripts in L<App::CekBpom>, for
+fewer dependencies.
+
+See included script L<cek-bpom-products> and L<cek-bpom-manufacturers>.
+
+
+=head1 SEE ALSO
+
+=cut
